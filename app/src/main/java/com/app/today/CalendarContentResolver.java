9031cdbf883ae2ba62e86f0710d874d75fb16ec6
taylorc1009 <taylorcourtney2000@googlemail.com>
2020-01-31 16:22:56 +0000

    Error while getting data from calendar: increment i to loop through Set<String> and store them in a CharSequence[]

---
+++ app/src/main/java/com/app/today/CalendarContentResolver.java
@@ -0,0 +1,38 @@
+package com.app.today;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.CalendarContract;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CalendarContentResolver {
+    public static final String[] FIELDS = {
+            CalendarContract.Calendars.NAME,
+            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,
+            CalendarContract.Calendars.CALENDAR_COLOR,
+            CalendarContract.Calendars.VISIBLE
+    };
+    public static final Uri CALENDAR_URI = Uri.parse("content://com.android.calendar/calendars");
+    ContentResolver contentResolver;
+    Set<String> calendars = new HashSet<String>();
+    public CalendarContentResolver(Context context) {
+        contentResolver = context.getContentResolver();
+    }
+    public Set<String> getCalendars() {
+        Cursor cursor = contentResolver.query(CALENDAR_URI, FIELDS, null, null, null);
+        try {
+            if(cursor.getCount() > 0) {
+                String name = cursor.getString(0);
+                String displayName = cursor.getString(1);
+                String color = cursor.getString(cursor.getColumnIndex(CalendarContract.Calendars.CALENDAR_COLOR));
+                Boolean selected = !cursor.getString(3).equals("0");
+                calendars.add(displayName);
+            }
+        } catch (AssertionError ex) {}
+        return calendars;
+    }
+}
--- app/src/main/java/com/app/today/MainActivity.java
+++ app/src/main/java/com/app/today/MainActivity.java
@@ -2,13 +2,36 @@ package com.app.today;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Date;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    @TargetApi(Build.VERSION_CODES.N)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        CalendarContentResolver calendar = new CalendarContentResolver(this);
+        Set<String> CalSet = calendar.getCalendars();
+        CharSequence[] instances = new CharSequence[CalSet.size()];
+        Integer i = new Integer(0);
+        CalSet.forEach((inst) -> {
+            instances[i] = inst;
+            i++;
+        });
+        /*String calendarInstance = calendar.getCalendars();
+        char[] instance = new char[calendarInstance.length()];
+        for(int i = 0; i < calendarInstance.length(); i++)
+            instance[i] = calendarInstance.charAt(i);*/
+        textView.setText(instance);
     }
 }
