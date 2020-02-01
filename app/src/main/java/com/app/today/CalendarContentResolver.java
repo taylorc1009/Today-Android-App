45cca29a4a54b5d1e4042e6875fdb7d400769f55
taylorc1009 <taylorcourtney2000@googlemail.com>
2020-02-01 17:21:51 +0000

    Second calendar retrieval test: + Permission request added + Target API changed from 13 to 23

--- app/src/main/java/com/app/today/CalendarContentResolver.java
+++ app/src/main/java/com/app/today/CalendarContentResolver.java
@@ -2,6 +2,7 @@ package com.app.today;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.CalendarContract;
@@ -19,20 +20,23 @@ public class CalendarContentResolver {
     public static final Uri CALENDAR_URI = Uri.parse("content://com.android.calendar/calendars");
     ContentResolver contentResolver;
     Set<String> calendars = new HashSet<String>();
+
     public CalendarContentResolver(Context context) {
         contentResolver = context.getContentResolver();
     }
+
     public Set<String> getCalendars() {
         Cursor cursor = contentResolver.query(CALENDAR_URI, FIELDS, null, null, null);
         try {
-            if(cursor.getCount() > 0) {
+            if (cursor.getCount() > 0) {
                 String name = cursor.getString(0);
                 String displayName = cursor.getString(1);
                 String color = cursor.getString(cursor.getColumnIndex(CalendarContract.Calendars.CALENDAR_COLOR));
                 Boolean selected = !cursor.getString(3).equals("0");
                 calendars.add(displayName);
             }
-        } catch (AssertionError ex) {}
+        } catch (AssertionError ex) {
+        }
         return calendars;
     }
 }
--- app/src/main/java/com/app/today/MainActivity.java
+++ app/src/main/java/com/app/today/MainActivity.java
@@ -1,8 +1,12 @@
 package com.app.today;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,18 +17,58 @@ import java.util.Date;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final int MY_PERMISSIONS_REQUEST_READ_CALENDAR = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView = (TextView) findViewById(R.id.textView);
-        CalendarContentResolver calendar = new CalendarContentResolver(this);
-        Set<String> CalSet = calendar.getCalendars();
-        CharSequence instances = "";
-        for(String s : CalSet) {
-            instances = instances + "," + s;
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Should we show an explanation?
+            textView.setText("CALENDAR PERMISSION REQUIRED");
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_CALENDAR)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_CALENDAR}, MY_PERMISSIONS_REQUEST_READ_CALENDAR);
+            }
+        } else {
+            // Permission has already been granted
         }
-        textView.setText(instances);
+        if (checkSelfPermission(Manifest.permission.READ_CALENDAR) == PackageManager.PERMISSION_GRANTED) {
+            CalendarContentResolver calendar = new CalendarContentResolver(this);
+            Set<String> CalSet = calendar.getCalendars();
+            CharSequence instances = "";
+            for (String s : CalSet) {
+                instances = instances + "," + s;
+            }
+            textView.setText(instances);
+        }
+    }
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CALENDAR: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Toast.makeText(MainActivity.this, "Calendar permission GRANTED", Toast.LENGTH_LONG);
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Calendar permission denied...", Toast.LENGTH_LONG);
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 }
