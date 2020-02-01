2a934585601caf24b44485738f218ff6cffdbdba
taylorc1009 <taylorcourtney2000@googlemail.com>
2020-02-01 13:27:54 +0000

    First calendar retrieval test

--- app/src/main/java/com/app/today/MainActivity.java
+++ app/src/main/java/com/app/today/MainActivity.java
@@ -15,23 +15,16 @@ import java.util.Set;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    @TargetApi(Build.VERSION_CODES.N)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView = (TextView) findViewById(R.id.textView);
         CalendarContentResolver calendar = new CalendarContentResolver(this);
         Set<String> CalSet = calendar.getCalendars();
-        CharSequence[] instances = new CharSequence[CalSet.size()];
-        Integer i = new Integer(0);
-        CalSet.forEach((inst) -> {
-            instances[i] = inst;
-            i++;
-        });
-        /*String calendarInstance = calendar.getCalendars();
-        char[] instance = new char[calendarInstance.length()];
-        for(int i = 0; i < calendarInstance.length(); i++)
-            instance[i] = calendarInstance.charAt(i);*/
-        textView.setText(instance);
+        CharSequence instances = "";
+        for(String s : CalSet) {
+            instances = instances + "," + s;
+        }
+        textView.setText(instances);
     }
 }
