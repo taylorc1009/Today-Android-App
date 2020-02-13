e2439977f2abccf86f6e27d77fbb87ba292ccb2f
taylorc1009 <taylorcourtney2000@googlemail.com>
2020-02-13 20:11:14 +0000

    Calendar retrieval working ? may not be pulling user events, it seems to only show events added by Google

--- app/src/main/java/com/app/today/MainActivity.java
+++ app/src/main/java/com/app/today/MainActivity.java
@@ -25,15 +25,21 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     static final int MY_PERMISSIONS_REQUEST_READ_CALENDAR = 0;
     static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-    TextView lastWUpdateTxt, forecastTxt, highsLowsTxt, temperatureTxt, windTxt;
+    static TextView lastWUpdateTxt, forecastTxt, highsLowsTxt, temperatureTxt, windTxt, event1Txt, event2Txt, event3Txt;
+
     List<String> weatherDetails = new ArrayList<>();
     static final String API = "2a2d2e85e492fe3c92b568f4fe3ce854";
     private double longitude, latitude;
+
+    public static List<String> nameOfEvent = new ArrayList<>();
+    public static List<String> startDates = new ArrayList<>();
+    public static List<String> endDates = new ArrayList<>();
+    public static List<String> descriptions = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,86 +49,82 @@ public class MainActivity extends AppCompatActivity {
         highsLowsTxt = findViewById(R.id.highsLows);
         temperatureTxt = findViewById(R.id.temperature);
         windTxt = findViewById(R.id.windSpeed);
+        event1Txt = findViewById(R.id.event1txt);
+        event2Txt = findViewById(R.id.event2txt);
+        event3Txt = findViewById(R.id.event3txt);
 
-        class weatherTask extends AsyncTask<String, Void, String> {
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-                findViewById(R.id.weatherLoad).setVisibility(View.VISIBLE);
-                findViewById(R.id.weatherGroup).setVisibility(View.GONE);
-                findViewById(R.id.weatherError).setVisibility(View.GONE);
-            }
-            @Override
-            public String doInBackground(String... args) {
-                //return HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=Edinburgh,GB&APPID=" + API);
-                final LocationListener locationListener = new LocationListener() {
-                    @Override
-                    public void onLocationChanged(Location location) {
-                        longitude = location.getLongitude();
-                        latitude = location.getLatitude();
-                    }
-                    @Override
-                    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                    }
-                    @Override
-                    public void onProviderEnabled(String provider) {
-
-                    }
-                    @Override
-                    public void onProviderDisabled(String provider) {
-
-                    }
-                };
-                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                    Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    HandlerThread t = new HandlerThread("handlerThread");
-                    t.start();
-                    lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener, t.getLooper());
-                    if(location != null)
-                        return HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=" + API);
-                        //return HttpRequest.excuteGet("https://api.darksky.net/" + API + "/" + latitude + "," + longitude);
-                        //return HttpRequest.excuteGet("https://api.darksky.net/forecast/fc7495b2595be4d034d506c6a16bcda3/37.8267,-122.4233");
-                }
-                return null;
-            }
-            @Override
-            protected void onPostExecute(String result) {
-                try {
-                    JSONObject jsonObj = new JSONObject(result);
-                    JSONObject main = jsonObj.getJSONObject("main");
-                    JSONObject wind = jsonObj.getJSONObject("wind");
-                    JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
-                    Long updatedAt = jsonObj.getLong("dt");
-                    weatherDetails.add(weather.getString("description"));
-                    weatherDetails.add(main.getString("temp") + "°C");
-                    weatherDetails.add(main.getString("temp_min") + "/" + main.getString("temp_max") + "°C");
-                    weatherDetails.add(wind.getString("speed"));
-                    weatherDetails.add("last updated: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000)));
-                    if (!weatherDetails.isEmpty()) {
-                        forecastTxt.setText(weatherDetails.get(0).toUpperCase());
-                        temperatureTxt.setText(weatherDetails.get(1));
-                        highsLowsTxt.setText(weatherDetails.get(2));
-                        windTxt.setText(weatherDetails.get(3));
-                        lastWUpdateTxt.setText(weatherDetails.get(4));
-                        findViewById(R.id.weatherLoad).setVisibility(View.GONE);
-                        findViewById(R.id.weatherGroup).setVisibility(View.VISIBLE);
-                    } else {
-                        findViewById(R.id.weatherLoad).setVisibility(View.GONE);
-                        findViewById(R.id.weatherError).setVisibility(View.VISIBLE);
-                    }
-                } catch (JSONException e) {
-                    findViewById(R.id.weatherLoad).setVisibility(View.GONE);
-                    findViewById(R.id.weatherError).setVisibility(View.VISIBLE);
-                }
-            }
-        }
         if (reqPermission(MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION))
             new weatherTask().execute(); //--> http request fails
         if (reqPermission(MY_PERMISSIONS_REQUEST_READ_CALENDAR)) {
             CalendarContentResolver calendar = new CalendarContentResolver(getApplicationContext());
-            Set<String> CalSet = calendar.getCalendar();
+            /*CalendarContentResolver calendar = new CalendarContentResolver();
+            calendar.getCalendar(getApplicationContext());*/
+        }
+    }
+    class weatherTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            findViewById(R.id.weatherLoad).setVisibility(View.VISIBLE);
+            findViewById(R.id.weatherGroup).setVisibility(View.GONE);
+            findViewById(R.id.weatherError).setVisibility(View.GONE);
+        }
+        @Override
+        public String doInBackground(String... args) {
+            //return HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=Edinburgh,GB&APPID=" + API);
+            final LocationListener locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    longitude = location.getLongitude();
+                    latitude = location.getLatitude();
+                }
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {}
+                @Override
+                public void onProviderEnabled(String provider) {}
+                @Override
+                public void onProviderDisabled(String provider) {}
+            };
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                HandlerThread t = new HandlerThread("handlerThread");
+                t.start();
+                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener, t.getLooper());
+                if(location != null)
+                    return HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=" + API);
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject jsonObj = new JSONObject(result);
+                JSONObject main = jsonObj.getJSONObject("main");
+                JSONObject wind = jsonObj.getJSONObject("wind");
+                JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
+                Long updatedAt = jsonObj.getLong("dt");
+                weatherDetails.add(weather.getString("description"));
+                weatherDetails.add(main.getString("temp") + "°C");
+                weatherDetails.add(main.getString("temp_min") + "°C min - " + main.getString("temp_max") + "°C max");
+                weatherDetails.add(wind.getString("speed") + " mph winds");
+                weatherDetails.add("last updated: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000)));
+                if (!weatherDetails.isEmpty()) {
+                    forecastTxt.setText(weatherDetails.get(0).toUpperCase());
+                    temperatureTxt.setText(weatherDetails.get(1));
+                    highsLowsTxt.setText(weatherDetails.get(2));
+                    windTxt.setText(weatherDetails.get(3));
+                    lastWUpdateTxt.setText(weatherDetails.get(4));
+                    findViewById(R.id.weatherLoad).setVisibility(View.GONE);
+                    findViewById(R.id.weatherGroup).setVisibility(View.VISIBLE);
+                } else {
+                    findViewById(R.id.weatherLoad).setVisibility(View.GONE);
+                    findViewById(R.id.weatherError).setVisibility(View.VISIBLE);
+                }
+            } catch (JSONException e) {
+                findViewById(R.id.weatherLoad).setVisibility(View.GONE);
+                findViewById(R.id.weatherError).setVisibility(View.VISIBLE);
+            }
         }
     }
     private boolean reqPermission(int p) {
