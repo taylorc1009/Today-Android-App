1a07bc257c28d5b4be1acca795e82e9801da28d9
taylorc1009 <taylorcourtney2000@googlemail.com>
2020-02-14 17:06:22 +0000

    Added alarm UI

---
+++ app/src/main/java/com/app/today/AlarmManager.java
@@ -0,0 +1,14 @@
+package com.app.today;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlarmManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_manager);
+    }
+}
