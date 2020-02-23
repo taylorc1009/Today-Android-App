2212b9cb95650b278d2cd3aec04d7b53dc171904
taylorc1009 <taylorcourtney2000@googlemail.com>
2020-02-23 15:50:34 +0000

    Added alarm ring activity + added alarm resources

--- app/src/main/java/com/app/today/AlarmRing.java
+++ app/src/main/java/com/app/today/AlarmRing.java
@@ -5,18 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AlarmRing extends AppCompatActivity {
     ImageView alarmIcon;
+    FloatingActionButton snoozeAlarm, stopAlarm;
+    TextView alarmTime, alarmLabel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_ring);
         alarmIcon = findViewById(R.id.alarmIcon);
-        alarmIcon.setMinimumWidth(200);
-        alarmIcon.setMinimumHeight(200);
+        snoozeAlarm = findViewById(R.id.snoozeAlarm);
+        stopAlarm = findViewById(R.id.stopAlarm);
+        alarmTime = findViewById(R.id.alarmTime);
+        alarmLabel = findViewById(R.id.alarmLabel);
+
+        //animate alarm icon to scale in and out
+
         MediaPlayer ringtone = MediaPlayer.create(this, R.raw.alarmheaven);
         ringtone.start();
-
     }
 }
