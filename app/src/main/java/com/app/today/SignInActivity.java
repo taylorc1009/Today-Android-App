92aba05f60bc475074a5beae8824991f5822430d
taylorc1009 <taylorcourtney2000@googlemail.com>
2020-02-25 21:29:36 +0000

    Began sign in page validation and Firebase integration

--- app/src/main/java/com/app/today/SignInActivity.java
+++ app/src/main/java/com/app/today/SignInActivity.java
@@ -4,7 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -13,19 +16,58 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static android.util.Patterns.EMAIL_ADDRESS;
+
 public class SignInActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    EditText email, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        String emailStr = null;
+        String passStr = null;
+
+        final Pattern p = Pattern.compile("[a-zA-Z][0-9]+");
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(p.matcher(s).matches() || s == "@" || s == ".") {
+                    email.getText().delete(email.getText().length() - 1, email.getText().length());
+                    Toast.makeText(SignInActivity.this, "Email may only consist of characters a-z/0-9", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
 
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s == "(" || s == ")" || s == "=" || s == "'" || s == "\"") {
+                    password.getText().delete(password.getText().length() - 1, password.getText().length());
+                    Toast.makeText(SignInActivity.this, "Password may not contain reserved characters", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
 
-        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        mAuth.createUserWithEmailAndPassword(emailStr, passStr).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
@@ -44,7 +86,7 @@ public class SignInActivity extends AppCompatActivity {
             }
         });
 
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        mAuth.signInWithEmailAndPassword(emailStr, passStr).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
@@ -67,5 +109,8 @@ public class SignInActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser user = mAuth.getCurrentUser();
         return user != null;
+    }
+    private boolean isValidEmail(String str) {
+        return EMAIL_ADDRESS.matcher(str).matches();
     }
 }
