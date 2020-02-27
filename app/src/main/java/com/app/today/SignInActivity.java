c491f3ae27ad44ff1ea1238154b074af20d9f984
taylorc1009 <taylorcourtney2000@googlemail.com>
2020-02-27 15:26:38 +0000

    Sign in credentials validation added

--- app/src/main/java/com/app/today/SignInActivity.java
+++ app/src/main/java/com/app/today/SignInActivity.java
@@ -7,6 +7,10 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.BaseInputConnection;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -24,49 +28,46 @@ import static android.util.Patterns.EMAIL_ADDRESS;
 public class SignInActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     EditText email, password;
+    Button register, signIn;
+
+    private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    private String emailStr = "";
+    private String passStr = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
-
-        String emailStr = null;
-        String passStr = null;
-
-        final Pattern p = Pattern.compile("[a-zA-Z][0-9]+");
+        register = findViewById(R.id.registerBtn);
+        signIn = findViewById(R.id.signInBtn);
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
-
-        email.addTextChangedListener(new TextWatcher() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(p.matcher(s).matches() || s == "@" || s == ".") {
-                    email.getText().delete(email.getText().length() - 1, email.getText().length());
-                    Toast.makeText(SignInActivity.this, "Email may only consist of characters a-z/0-9", Toast.LENGTH_SHORT).show();
-                }
+            public void onClick(View v) {
+                //if(isValidEmail(emailStr)) <-- copy email in EditText to register page?
             }
-            @Override
-            public void afterTextChanged(Editable s) {}
         });
-
-        password.addTextChangedListener(new TextWatcher() {
+        signIn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s == "(" || s == ")" || s == "=" || s == "'" || s == "\"") {
-                    password.getText().delete(password.getText().length() - 1, password.getText().length());
-                    Toast.makeText(SignInActivity.this, "Password may not contain reserved characters", Toast.LENGTH_SHORT).show();
-                }
+            public void onClick(View v) {
+                emailStr = email.getText().toString();
+                passStr = password.getText().toString();
+                if(!passStr.equals("") && isValidPassword(passStr)) {
+                    if(!emailStr.equals("") && isValidEmail(emailStr)) {
+                        //attempt sign in
+                    } else
+                        Toast.makeText(SignInActivity.this, "Invalid email, please re-enter", Toast.LENGTH_LONG).show();
+                } else
+                    Toast.makeText(SignInActivity.this, "Please enter your password", Toast.LENGTH_LONG).show();
             }
-            @Override
-            public void afterTextChanged(Editable s) {}
         });
-
+        /*assert emailStr != null;
+        assert passStr != null;
         mAuth.createUserWithEmailAndPassword(emailStr, passStr).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -103,7 +104,7 @@ public class SignInActivity extends AppCompatActivity {
 
                 // ...
             }
-        });
+        });*/
     }
     public boolean isSignedIn() {
         // Check if user is signed in (non-null) and update UI accordingly.
@@ -112,5 +113,14 @@ public class SignInActivity extends AppCompatActivity {
     }
     private boolean isValidEmail(String str) {
         return EMAIL_ADDRESS.matcher(str).matches();
+    }
+    private boolean isValidPassword(String str) {
+        for(int i = 0; i < str.length(); i++)
+            if(!isValidChar(str.charAt(i)))
+                return false;
+        return true;
+    }
+    private boolean isValidChar(char c) {
+        return c != '(' && c != ')' && c != '\"' && c != '=' && c != '\'' && c != '\\';
     }
 }
