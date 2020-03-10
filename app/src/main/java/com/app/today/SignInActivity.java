b96d38851c6b281977ebb64d928b2e11172130ef
taylorc1009 <taylorcourtney2000@googlemail.com>
2020-03-10 20:46:56 +0000

    Finished the SignInActivity internal commentary, removed the '=' char from password validation and added '%' and '/'

--- app/src/main/java/com/app/today/SignInActivity.java
+++ app/src/main/java/com/app/today/SignInActivity.java
@@ -19,12 +19,11 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
-
 import java.util.Objects;
-
 import static android.util.Patterns.EMAIL_ADDRESS;
 
 public class SignInActivity extends AppCompatActivity {
+    //UI attributes
     EditText email, password, realName;
     Button signIn;
     ConstraintLayout credentialsGroup, registerExpand, registerGroup, authenticating;
@@ -32,8 +31,10 @@ public class SignInActivity extends AppCompatActivity {
     ProgressBar authLoad;
     TextView authTxt;
 
+    //Create an instance of Firebase authenticator
     protected static FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
+    //Local sign-in/register variables
     private String emailStr = "";
     private String passStr = "";
     private String displayName = "";
@@ -43,7 +44,11 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        //Hide action bar
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        //Initialize the UI attributes
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         regArrow = findViewById(R.id.regArrow);
@@ -55,15 +60,22 @@ public class SignInActivity extends AppCompatActivity {
         authTxt = findViewById(R.id.authTxt);
         realName = findViewById(R.id.realName);
         signIn = findViewById(R.id.signInBtn);
+
         cleanView();
+
+        //Button to change to register an account instead of signing in
         registerExpand.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //If user is not already registering, enter register mode
+                //Else exit register mode
                 if(!isRegistering) {
                     isRegistering = true;
+                    //Animates the reveal of the register views
                     regArrow.animate().rotation(180);
                     registerGroup.setVisibility(View.VISIBLE);
                     registerGroup.animate().alpha(1);
+                    //Changes the sign-in button text
                     signIn.setText(R.string.registerBtn);
                 }
                 else {
@@ -72,11 +84,19 @@ public class SignInActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //Button used to sign-in/register
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //If password is not blank and is valid, continue
+                //Else request re-entry
                 if(!password.getText().toString().trim().equals("") && isValidPassword(password.getText().toString().trim())) {
+                    //If email is not blank and is valid, continue
+                    //Else request re-entry
                     if(!email.getText().toString().trim().equals("") && isValidEmail(email.getText().toString().trim())) {
+                        //If the user is registering, validate their display name then attempt to register them
+                        //Else attempt sign in
                         if(isRegistering) {
                             if(!realName.getText().toString().trim().equals("") && isValidName(realName.getText().toString().trim())) {
                                 emailStr = email.getText().toString().trim();
@@ -101,34 +121,51 @@ public class SignInActivity extends AppCompatActivity {
             }
         });
     }
+
+    //Method used to register the user using Firebase
     private void createAccount() {
+        //Set the signing in view text to...
         authTxt.setText(R.string.authReg);
+
+        //Utilizes the Firebase create account task to create the users account
         mAuth.createUserWithEmailAndPassword(emailStr, passStr).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+                //If the registration was successful, update the local sign in details of the app then go to the MainActivity
+                //Else display an error and return to the sign in page
                 if(task.isSuccessful()) {
                     Log.d("Firebase Register", "createUserWithEmail:success");
                     FirebaseUser user = mAuth.getCurrentUser();
+
+                    //Set the users display name to what they entered upon registering
                     UserProfileChangeRequest request = new UserProfileChangeRequest.Builder().setDisplayName(displayName).build();
                     assert user != null;
                     user.updateProfile(request);
+
                     Intent mainActivity = new Intent(SignInActivity.this, MainActivity.class);
                     startActivity(mainActivity);
                     finish();
                 } else {
                     Log.w("Firebase Register", "createUserWithEmail:failure", task.getException());
-                    Toast.makeText(SignInActivity.this, "Account not found", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignInActivity.this, "Failed to register account...", Toast.LENGTH_SHORT).show();
                     authenticating.setVisibility(View.GONE);
                     credentialsGroup.setVisibility(View.VISIBLE);
                 }
             }
         });
     }
+
+    //Method used to sign-in an existing user
     private void signIn() {
+        //Set the signing in view text to..
         authTxt.setText(R.string.authSign);
+
+        //Utilizes the Firebase sing-in task to sign the user into an existing account
         mAuth.signInWithEmailAndPassword(emailStr, passStr).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+                //If the sign-in was successful, send the user to the main activity
+                //Else display an error and return them to the sign in page
                 if(task.isSuccessful()) {
                     Log.d("Firebase Login", "signInWithEmail:success");
                     Intent mainActivity = new Intent(SignInActivity.this, MainActivity.class);
@@ -136,33 +173,45 @@ public class SignInActivity extends AppCompatActivity {
                     finish();
                 } else {
                     Log.w("Firebase Login", "signInWithEmail:failure", task.getException());
-                    Toast.makeText(SignInActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignInActivity.this, "Authentication failed...", Toast.LENGTH_SHORT).show();
                     authenticating.setVisibility(View.GONE);
                     credentialsGroup.setVisibility(View.VISIBLE);
                 }
             }
         });
     }
+
+    //Used to determine whether the users email matches the traditional email format using Android's
+    //EMAIL_ADDRESS utility
     private boolean isValidEmail(String str) {
         return EMAIL_ADDRESS.matcher(str).matches();
     }
+
+    //Used to check the users password
     private boolean isValidPassword(String str) {
+        //For every char in the password field, check it it is a valid char
         for(int i = 0; i < str.length(); i++)
             if(!isValidChar(str.charAt(i)))
                 return false;
         return true;
     }
+    //Used to check each char in a password, mainly to check for illegal, syntax-altering chars
     private boolean isValidChar(char c) {
-        return c != '(' && c != ')' && c != '\"' && c != '=' && c != '\'' && c != '\\' && c != ' ';
+        return c != '(' && c != ')' && c != '\"' && c != '%' && c != '\'' && c != '\\' && c != '/' && c != ' ';
     }
+
+    //Used to determine if the display name is valid by:
     private boolean isValidName(String str) {
         boolean result = true;
         String[] indexes;
         indexes = str.split(" ");
+        //Checking if there is more than one word in the name or the name does not match a regex of a-z and A-Z
         if(indexes.length != 1 || !str.matches("[A-Za-z]+"))
             result = false;
         return result;
     }
+
+    //Used to clean the register views
     private Runnable cleanView() {
         if(isRegistering) {
             isRegistering = false;
