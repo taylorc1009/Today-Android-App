96a41f6d8fdb28547ec951409251d2f31f659727
taylorc1009 <taylorcourtney2000@googlemail.com>
2020-03-11 10:01:18 +0000

    Internal commentary almost finished, just need to complete AlarmActivity and DatabaseUtilities

--- app/src/main/java/com/app/today/AlarmRing.java
+++ app/src/main/java/com/app/today/AlarmRing.java
@@ -7,10 +7,13 @@ import android.util.Log;
 import java.util.Objects;
 
 public class AlarmRing extends BroadcastReceiver {
+    //This is our alarm receiver, the intent we set in the AlarmManager is instructed to broadcast to this
+    //The receiver is defined in the AndroidManifest
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i("! alarm PendingIntent received with ID", Objects.requireNonNull(Objects.requireNonNull(intent.getExtras()).getString("alarmID")));
         if (intent.hasExtra("alarmID")) {
+            //Start the AlarmActivity with the alarm ID specified
             Intent sendAlarmIntent = new Intent(context, AlarmActivity.class);
             sendAlarmIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             sendAlarmIntent.putExtra("alarmID", intent.getExtras().getString("alarmID"));
--- app/src/main/java/com/app/today/DateUtilities.java
+++ app/src/main/java/com/app/today/DateUtilities.java
@@ -1,29 +1,20 @@
 package com.app.today;
 
-import android.app.Activity;
-import android.content.Context;
 import android.icu.util.Calendar;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 
 class DateUtilities {
+    //I was going to make a collection of date utilities for the calendar and alarms, but this was the only method I needed across both
     static Calendar buildTime(int hour, int minute, int second, int millis) {
+        //Create an instance of the exact current time in milliseconds
         Calendar day = Calendar.getInstance();
         day.setTimeInMillis(System.currentTimeMillis());
+
+        //Alter said time using the parameters to build a time instance the system can use for calculations
         day.set(Calendar.HOUR_OF_DAY, hour);
         day.set(Calendar.MINUTE, minute);
         day.set(Calendar.SECOND, second);
         day.set(Calendar.MILLISECOND, millis);
+
         return day;
-    }
-    public static void hideKeyboardFrom(Context context, View view) {
-        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
-        //Find the currently focused view, so we can grab the correct window token from it.
-        /*View view = context.getCurrentFocus();
-        //If no view currently has focus, create a new one, just so we can grab a window token from it
-        if (view == null) {
-            view = new View(activity);
-        }*/
-        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
